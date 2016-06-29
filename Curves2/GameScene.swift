3abdaa08e6b9c0b55a02490cfc66243a93b6e77a
Moritz Martin <moritz.martin@uni-ulm.de>
2016-06-29 10:48:52 +0200

    Buttons

--- Curves2/GameScene.swift
+++ Curves2/GameScene.swift
@@ -8,8 +8,13 @@
 
 import SpriteKit
 
+struct GameData{
+    static var colors = [UIColor]()
+}
+
 class GameScene: SKScene {
     
+    let trianglePathP1L = CGPathCreateMutable()
     
     //unten Links
     var p1R = SKShapeNode()
@@ -31,85 +36,28 @@ class GameScene: SKScene {
     var p4L = SKShapeNode()
     
     
+    var gameArea = SKShapeNode()
+    var btnWidth: CGFloat = 110
+    
+    var players: Int = 3
+
+    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
        
         scaleMode = .ResizeFill
-    
-        
-        let trianglePathP1L = CGPathCreateMutable()
-        CGPathMoveToPoint(trianglePathP1L, nil, 5, 100)
-        CGPathAddLineToPoint(trianglePathP1L, nil, 105, 100)
-        CGPathAddLineToPoint(trianglePathP1L, nil, 5, 0)
-        CGPathCloseSubpath(trianglePathP1L)
-        
-        //1 Unten Links
-        p1L = SKShapeNode(path: trianglePathP1L)
-        p1L.position = CGPoint(x: 0, y: 10)
-        p1L.strokeColor = SKColor.blueColor()
-        p1L.fillColor = SKColor.redColor()
-        self.addChild(p1L)
-        
-        p1R = SKShapeNode(path: trianglePathP1L)
-        p1R.position = CGPoint(x: 5 + 110, y: 105)
-        p1R.strokeColor = SKColor.blueColor()
-        p1R.fillColor = SKColor.redColor()
-        p1R.zRotation = CGFloat(-M_PI_4)*4
-        self.addChild(p1R)
+        createButtons(players)
         
         
         
-        //2 Oben links
-        p2R = SKShapeNode(path: trianglePathP1L)
-        p2R.position = CGPoint(x: 105, y: view.frame.height - 115)
-        p2R.strokeColor = SKColor.blueColor()
-        p2R.zRotation = CGFloat(M_PI_2)
-        p2R.fillColor = SKColor.greenColor()
-        self.addChild(p2R)
         
-        p2L = SKShapeNode(path: trianglePathP1L)
-        p2L.position = CGPoint(x: 10, y: view.frame.height)
-        p2L.strokeColor = SKColor.blueColor()
-        p2L.zRotation = CGFloat(-M_PI_2)
-        p2L.fillColor = SKColor.redColor()
-        self.addChild(p2L)
-        
-        
-        
-        // 3 oben rechtss
-        p3R = SKShapeNode(path: trianglePathP1L)
-        p3R.position = CGPoint(x:view.frame.width - 115, y: view.frame.height - 105)
-        p3R.strokeColor = SKColor.blueColor()
-        p3R.fillColor = SKColor.greenColor()
-        self.addChild(p3R)
-        
-        
-        p3L = SKShapeNode(path: trianglePathP1L)
-        p3L.position = CGPoint(x: view.frame.width, y: view.frame.height - 10)
-        p3L.strokeColor = SKColor.blueColor()
-        p3L.zRotation = CGFloat(M_PI)
-        p3L.fillColor = SKColor.redColor()
-        self.addChild(p3L)
-        
-        
-        
-        // 4 unten
-        p4L = SKShapeNode(path: trianglePathP1L)
-        p4L.position = CGPoint(x: view.frame.width - 10 , y: 0)
-        p4L.strokeColor = SKColor.blueColor()
-        p4L.zRotation = CGFloat (M_PI_2)
-        p4L.fillColor = SKColor.greenColor()
-        self.addChild(p4L)
-        
-        
-        p4R = SKShapeNode(path: trianglePathP1L)
-        p4R.position = CGPoint(x: view.frame.width - 105 , y: 115)
-        p4R.strokeColor = SKColor.blueColor()
-        p4R.zRotation = CGFloat (-M_PI_2)
-        p4R.fillColor = SKColor.greenColor()
-        self.addChild(p4R)
-        
+        gameArea = SKShapeNode(rect: CGRect(x: btnWidth + 5 , y: 5, width: view.frame.width - (2*btnWidth+10), height: view.frame.height - 10))
+        gameArea.lineWidth = 5
+        gameArea.strokeColor = SKColor.whiteColor()
+        self.addChild(gameArea)
     }
+    
+    
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
         
@@ -146,31 +94,96 @@ class GameScene: SKScene {
         /* Called before each frame is rendered */
     }
     
+    
+    func createButtons(playerCount: Int){
+        
+        
+        CGPathMoveToPoint(trianglePathP1L, nil, 5, 100)
+        CGPathAddLineToPoint(trianglePathP1L, nil, 105, 100)
+        CGPathAddLineToPoint(trianglePathP1L, nil, 5, 0)
+        CGPathCloseSubpath(trianglePathP1L)
+        
+        
+        
+        
+        if playerCount == 2{
+            //Ganz andere Anordnung
+        }
+        
+        
+        if playerCount > 2 {
+            
+            p1L = SKShapeNode(path: trianglePathP1L)
+            p1L.position = CGPoint(x: 0, y: 10)
+            p1L.strokeColor = SKColor.blueColor()
+            p1L.fillColor = SKColor.redColor()
+            self.addChild(p1L)
+            
+            p1R = SKShapeNode(path: trianglePathP1L)
+            p1R.position = CGPoint(x: 5 + 110, y: 105)
+            p1R.strokeColor = SKColor.blueColor()
+            p1R.fillColor = SKColor.redColor()
+            p1R.zRotation = CGFloat(-M_PI_4)*4
+            self.addChild(p1R)
+            
+            
+            p3L = SKShapeNode(path: trianglePathP1L)
+            p3L.position = CGPoint(x: view!.frame.width - 10 , y: 0)
+            p3L.strokeColor = SKColor.blueColor()
+            p3L.zRotation = CGFloat (M_PI_2)
+            p3L.fillColor = SKColor.greenColor()
+            self.addChild(p3L)
+            
+            p3R = SKShapeNode(path: trianglePathP1L)
+            p3R.position = CGPoint(x: view!.frame.width - 105 , y: 115)
+            p3R.strokeColor = SKColor.blueColor()
+            p3R.zRotation = CGFloat (-M_PI_2)
+            p3R.fillColor = SKColor.greenColor()
+            p3R.alpha = 0.3
+            self.addChild(p3R)
+            
+            
+            p2R = SKShapeNode(path: trianglePathP1L)
+            p2R.position = CGPoint(x:view!.frame.width - 115, y: view!.frame.height - 105)
+            p2R.strokeColor = SKColor.blueColor()
+            p2R.fillColor = SKColor.greenColor()
+            self.addChild(p2R)
+            
+            p2L = SKShapeNode(path: trianglePathP1L)
+            p2L.position = CGPoint(x: view!.frame.width, y: view!.frame.height - 10)
+            p2L.strokeColor = SKColor.blueColor()
+            p2L.zRotation = CGFloat(M_PI)
+            p2L.fillColor = SKColor.redColor()
+            self.addChild(p2L)
+            
+
+            
+        }
+        if playerCount > 3 {
+            
+
+            p4R = SKShapeNode(path: trianglePathP1L)
+            p4R.position = CGPoint(x: 105, y: view!.frame.height - 115)
+            p4R.strokeColor = SKColor.blueColor()
+            p4R.zRotation = CGFloat(M_PI_2)
+            p4R.fillColor = SKColor.greenColor()
+            self.addChild(p4R)
+            
+            p4L = SKShapeNode(path: trianglePathP1L)
+            p4L.position = CGPoint(x: 10, y: view!.frame.height)
+            p4L.strokeColor = SKColor.blueColor()
+            p4L.zRotation = CGFloat(-M_PI_2)
+            p4L.fillColor = SKColor.redColor()
+            self.addChild(p4L)
+                        
+        }
+        
+        
+        
+        
+    }
+    
+    
+    
+    
 }
-
-
-//
-//
-//func createButtons(){
-//    
-//    
-//    let trianglePathP1L = CGPathCreateMutable()
-//    CGPathMoveToPoint(trianglePathP1L, nil, 1, 105)
-//    CGPathAddLineToPoint(trianglePathP1L, nil, 100, 205)
-//    CGPathAddLineToPoint(trianglePathP1L, nil, 1, 205)
-//    CGPathCloseSubpath(trianglePathP1L)
-//    
-//    let p1L = SKShapeNode(path: trianglePathP1L)
-//    p1L.strokeColor = SKColor.blueColor()
-//    p1L.fillColor = SKColor.redColor()
-//    self.addChild(p1L)
-//    
-//    
-//    
-//    let p1R = SKShapeNode(path: trianglePathP1L)
-//    p1R.strokeColor = SKColor.blueColor()
-//    p1R.fillColor = SKColor.redColor()
-//    p1R.zRotation = CGFloat(-M_PI_4)*4
-//    p1R.position = CGPoint(x: 100, y: 300)
-//    self.addChild(p1R)
-//}
